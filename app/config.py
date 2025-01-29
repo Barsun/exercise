1c6b2598e0adc514284516434965d1e38c7e1c20
Denis <baciudenis@gmail.com>
2025-01-29 00:08:08 +0000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///mydatabase.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///mydatabase.db")
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
